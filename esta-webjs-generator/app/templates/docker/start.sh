157b861705dd14fa4ce97f218983c789399f9e9d
Reto Lehmann <reto.lehmann@sbb.ch>
2016-04-18 10:07:08 +0200

    Fixed sed to new bundle filename

--- esta-webjs-generator/app/templates/docker/start.sh
+++ esta-webjs-generator/app/templates/docker/start.sh
@@ -2,7 +2,7 @@
 
 # replace static values with environment-variables
 if [ -n "$AUTHSERVER_URL" ]; then
-    sed -i "s#http://localhost/#$AUTHSERVER_URL#g" /usr/share/nginx/html/bundle.js
+    sed -i "s#http://localhost/#$AUTHSERVER_URL#g" /usr/share/nginx/html/app.*.js
 fi
 
 # start webserver
