0d7a0d707e8bc54419c58bcce972140125a99ba3
u220374 <reto.lehmann@sbb.ch>
2016-08-15 12:44:23 +0200

    [ESTA-2123] Removed unnecessary 'copyHtml' gulp task

--- esta-webjs-generator/app/templates/gulpfile.babel.js
+++ esta-webjs-generator/app/templates/gulpfile.babel.js
@@ -53,13 +53,6 @@ let paths = {
 };
 
 /**
- * Gulp-Task: Kopiert alle statischen Dateien nach /dist
- */
-gulp.task('copyHtml', () => {
-    return gulp.src(path.join(root, 'index.html')).pipe(gulp.dest(paths.output));
-});
-
-/**
  * Gulp-Task: Fuehrt webpack aus und startet den Development-Server
  */
 gulp.task('serve', () => {
@@ -136,7 +129,7 @@ gulp.task('doc', () => {
  * - Kopiert index.html nach /target/build
  */
 gulp.task('build', ['test-selenium-webgrid', 'doc'], (done) => {
-    gulp.src(path.join(root, 'index.html')).pipe(gulp.dest(paths.build));
+    // gulp.src(path.join(root, 'index.html')).pipe(gulp.dest(paths.build));
 
     return webpack(webpackConfig.production, done);
 });
@@ -144,4 +137,4 @@ gulp.task('build', ['test-selenium-webgrid', 'doc'], (done) => {
 /**
  * Gulp-Task: Standardfall mit 'gulp' kopiert das HTML nach /dist und startet Webserver
  */
-gulp.task('default', ['copyHtml', 'serve']);
+gulp.task('default', ['serve']);
