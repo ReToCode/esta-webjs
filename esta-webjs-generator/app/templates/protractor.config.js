50942405b954f7b810fed392f97de1c4a6dafb72
U218609 <kevin.kreuzer@sbb.ch>
2017-06-07 12:09:52 +0200

    kk: Remove unnessecarry comment

--- esta-webjs-generator/app/templates/protractor.config.js
+++ esta-webjs-generator/app/templates/protractor.config.js
@@ -3,6 +3,6 @@ exports.config = {
     onPrepare: function onPrepare() {
         require('babel-core/register');
         browser.manage().window().setSize(1280, 1024);
-        console.log('We are going to use the following baseUrl during the tests:', browser.baseUrl); // eslint-disable-line no-console
+        console.log('We are going to use the following baseUrl during the tests:', browser.baseUrl);
     }
 }
